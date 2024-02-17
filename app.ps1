a9c2c8c6866bda6f9f8d01ece53ad01b067a3f79
Joel Shadbolt <joelshadbolt123@hotmail.com>
2024-02-17 22:15:56 +1000

    event logs

--- app.ps1
+++ app.ps1
@@ -3,26 +3,29 @@
 . "$PSScriptRoot/util/cloudwatch.ps1"
 . "$PSScriptRoot/util/s3.ps1"
 . "$PSScriptRoot/system/windows_info.ps1"
-. "$PSScriptRoot/system/dns_logs.ps1"
+. "$PSScriptRoot/dns/dns_logs.ps1"
+
+
 
 $hostname = getHost
 function Start-LogCollection {
     while ($true) {
-        $collectedLogs = Get-WindowsEventLogs
+        $eventData, $systemCrashLogs = Get-WindowsEventLogs
         $dnsLogs = Get-DNSLogs
 
-        foreach ($log in $collectedLogs) {
-            if ($log.Category -eq "Application" -or $log.Category -eq "System") {
-                Send-ToCloudWatch -Log $log
-            } else {
-                Upload-ToS3 -Log $log
-            }
+        if ($eventData) {
+            $logData = $eventData | Format-Table | Out-String
+            Send-ToCloudWatch -logData $logData -logGroupName "AppStream/"+$hostname+"- Event Logs" -logStreamName "Event-$(Get-Date -Format o)"
         }
-        
+
+        if ($systemCrashLogs) {
+            $logData = $systemCrashLogs | Format-Table | Out-String
+            Send-ToCloudWatch -logData $logData -logGroupName "AppStream/"+$hostname+$hostname+"- System Crash Logs" -logStreamName "SystemCrash-$(Get-Date -Format o)"
+        }
         
         if ($dnsLogs) {
             $logData = $dnsLogs | Format-Table | Out-String
-            Send-ToCloudWatch -logData $logData -logGroupName $hostname+"- DNS Logs" -logStreamName "DNS-$(Get-Date -Format o)"
+            Send-ToCloudWatch -logData $logData -logGroupName "AppStream/"+$hostname+$hostname+"- DNS Logs" -logStreamName "DNS-$(Get-Date -Format o)"
 }
 
         Start-Sleep -Seconds 300
@@ -30,3 +33,4 @@ function Start-LogCollection {
 }
 
 Start-LogCollection
+```
--- util/s3.ps1
+++
@@ -1,7 +0,0 @@
-function Upload-ToS3 {
-    param (
-        [string]$filePath,
-        [string]$bucketName
-    )
-    Write-S3Object -BucketName $bucketName -File $filePath -Key (Split-Path $filePath -Leaf)
-}
